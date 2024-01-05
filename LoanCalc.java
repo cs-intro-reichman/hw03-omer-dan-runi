de420696a613e9ca9f771c475303417fc23380be
omer-dan-runi <omer.dan01@post.runi.ac.il>
2024-01-05 20:09:17 +0200

    Update LoanCalc.java

--- LoanCalc.java
+++ LoanCalc.java
@@ -102,7 +102,7 @@ public class LoanCalc {
 	private static double endBalance(double loan, double rate, int n, double payment) {
         for (int i = 0; i < n; i++)
 		{
-			loan = (loan - payment) * (1 + rate);
+			loan = (loan - payment) * (1 + (rate/100));
         }
         return loan;
     }
